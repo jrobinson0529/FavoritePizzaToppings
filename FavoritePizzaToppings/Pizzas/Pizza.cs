3305c2c390e5149c6344d3bb0a41f884e2d19894
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-08-06 09:39:32 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Pizzas/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoritePizzaToppings.Pizzas
+{
+    class Pizza
+    {
+        [JsonProperty("toppings")]
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,37 @@
+﻿using FavoritePizzaToppings.Pizzas;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FavoritePizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllText(@"C:\Users\Jesse\Downloads\pizzas.json");
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(file);
+
+            var toppingsList = pizzas.GroupBy(pizza => String.Join(',', pizza.Toppings.OrderBy(x => x)));
+
+            var toppingCountList = new List<(string toppingName, int count)>();
+            foreach (var toppingCombo in toppingsList)
+            {
+                string name = toppingCombo.Key;
+                int count = toppingCombo.ToList().Count;
+
+                toppingCountList.Add((name, count));
+            }
+            var toppingCountListOrdered = toppingCountList.OrderByDescending(x => x.count);
+            var top20ToppingCombos = toppingCountListOrdered.Take(20).ToList();
+            var place = 1;
+            foreach (var topping in top20ToppingCombos)
+            {
+                Console.WriteLine($"{(place == 1 ? "1st" : place == 2 ? "2nd" : place == 3 ? "3rd" : place + "th")} - {topping.toppingName}: has been ordered {topping.count} times");
+                place++;
+            }
+        }
+    }
+}
